000c09bf89b64d4e62a43433addd415ac37d086d
Alex.Cai <cyy0523xc@gmail.com>
2011-12-12 16:11:40 +0800

    *

--- test.php
+++ test.php
@@ -1,23 +1,28 @@
 <?php
-echo phpinfo();
+$you = 'Special people';
+$hrEmail = 'hr1@youmi.net';
 
-/**
- * 
- * @param unknown_type $a
- */
-function test ($a)
+if (isCarriesDreams($you)              //怀揣梦想
+        && isImmoderation($you)        //不甘平庸
+        && isInnovation($you)          //坚持创新
+        && isAggressive($you)          //不断进取
+        && isEnjoyChallenge($you)      //喜欢挑战自我
+        )
 {
-    echo $a;
-    return $a;
+    pleaseSendYourResumeTo($hrEmail);
 }
 
-class A
-{
 
-    private $a = 'aa';
 
-    function test ($a)
-    {
-        $this->a = $a;
-    }
+function isCarriesDreams($you){}
+function isImmoderation($you){
 }
+function isAggressive($you){
+}
+function isEnjoyChallenge($you){
+}
+function isInnovation($you){
+}
+function pleaseSendResume($you){
+}
+?>
