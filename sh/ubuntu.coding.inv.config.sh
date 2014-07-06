0e1fac3639a2c4b738fcea3584c3c2afa450908a
Alex Cai <cyy0523xc@gmail.com>
2014-07-06 22:14:54 +0800

    编程环境初始化

---
+++ sh/ubuntu.coding.inv.config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo mkdir /home/code
+chmod -R alex:alex /home/code
+
+cd /home/code 
+
+# github
+mkdir github
+cd github
+git clone git@github.com:cyy0523xc/code.git
+git clone git@github.com:cyy0523xc/R.git
+git clone git@github.com:cyy0523xc/raw.git
+
+# ibbd
+cd /home/code/
+mkdir ibbd
+cd ibbd
+git clone git@git.ibbd.net:ibbd/ibbdcrm.git
+git clone git@git.ibbd.net:caiyingyao/cyy-doc.git
+git clone git@git.ibbd.net:caiyingyao/raw.git
+git clone git@git.ibbd.net:ibbd/juketong-intl.git
+git clone git@git.ibbd.net:ibbd/juketong-cafe-deco.git
+git clone git@git.ibbd.net:ibbd/ibbdcrm-tasks.git
+
+
+
+
+
