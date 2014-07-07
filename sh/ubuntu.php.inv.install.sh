410ac39c10a92bd1655c749059200bc5b927c3d7
alex cai <cyy0523xc@gmail.com>
2014-07-07 17:04:47 +0800

    *

---
+++ sh/ubuntu.php.inv.install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#sudo apt-get install nginx
+#sudo apt-get install mysql-server
+#sudo apt-get install php5
+
+wget -c http://soft.vpser.net/lnmp/lnmp1.1-full.tar.gz && tar zxf lnmp1.1-full.tar.gz && cd lnmp1.1-full && ./ubuntu.sh
