7716234946af98e902a9bd00586ddeb843f8c589
alex cai <cyy0523xc@gmail.com>
2014-07-08 14:47:58 +0800

    elasticsearch

--- sh/ubuntu.elasticsearch.install.sh
+++ sh/ubuntu.elasticsearch.install.sh
@@ -22,5 +22,20 @@ echo "kibana download OK"
 tar -vxf kibana*tar.gz
 echo "kibana tar OK"
 
+cd elasticsearch-1.2.1/
+
+# es2unix
+curl -s download.elasticsearch.org/es2unix/es > /home/alex/programs/elasticsearch-1.2.1/bin/es 
+chmod +x bin/es
+echo "es2unix OK"
+
+# jetty
+# @see https://github.com/sonian/elasticsearch-jetty
+elasticsearch-1.2.1/bin/plugin -url https://oss-es-plugins.s3.amazonaws.com/elasticsearch-jetty/elasticsearch-jetty-1.2.1.zip -install elasticsearch-jetty-1.2.1
+echo ""
+echo "# jetty config"
+echo "http.type: com.sonian.elasticsearch.http.jetty.JettyHttpServerTransportModule" >> config/elasticsearch.yml
+echo "elasticsearch-jetty OK"
 
 echo "---All is OK."
+
