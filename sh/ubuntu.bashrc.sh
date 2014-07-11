a195d9e7a5d344f81e3341e47771421b26c24e69
alex cai <cyy0523xc@gmail.com>
2014-07-11 09:44:46 +0800

    gitlab

--- sh/ubuntu.bashrc.sh
+++ sh/ubuntu.bashrc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 自定义bashrc
+# 在~/.bashrc中被引用
 
 alias ssh179='ssh root@112.124.47.179'
 alias ssh195='ssh root@115.29.166.195'
@@ -9,7 +10,7 @@ alias ssh35='ssh root@42.120.21.35'
 alias yuic="java -jar /home/code/ibbd/cyy-doc/tools/yuicompressor-2.4.7.jar "
 
 # elasticsearch
-alias es="/home/alex/programs/elasticsearch-1.2.1/bin/elasticsearch"
+alias es="/home/alex/programs/elasticsearch-1.2.2/bin/elasticsearch"
 
 # es的get数据接口
 # 用法：esget cafe order '{...}'
@@ -23,4 +24,36 @@ esget() {
     echo ""
 }
 
+cd_github_code() {
+    cd /home/code/github/
+}
+
+cd_ibbd_code() {
+    cd /home/code/ibbd/ 
+}
+
+# gitlab: new project
+# 参数说明：
+#   $1: project name
+#   $2: group name, the default value is "ibbd"
+ibbd_git_init() {
+    if [2 == $#]; then
+        groupname=$2
+    else
+        groupname="ibbd"
+    fi
+
+    cd_github_code
+    mkdir $1
+    cd $1
+    git init
+    touch README
+    git add README
+    git commit -m 'first commit'
+    git remote add origin git@git.ibbd.net:${groupname}/$1\.git
+    git push -u origin master
+
+    echo ""
+    echo "project $1 is OK!"
+}
 
