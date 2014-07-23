499e97488d48649a1e518240559210921266ad0d
alex cai <cyy0523xc@gmail.com>
2014-07-23 14:30:48 +0800

    es

--- sh/ubuntu.elasticsearch.install.sh
+++ sh/ubuntu.elasticsearch.install.sh
@@ -5,11 +5,11 @@
 # elasticsearch的版本：1.2.2 
 # kibana的版本：3.1.0 
 #
-# wget https://raw.githubusercontent.com/cyy0523xc/code/master/sh/ubuntu.elasticsearch.install.sh
+# wget https://raw.githubusercontent.com/cyy0523xc/code/master/sh/ubuntu.elasticsearch.install.sh | bash ubuntu.elasticsearch.install.sh
 #
 # 目录，可以指定其他的目录
 # NOTICES: 如果目录不同，则注意修改这里
-cd /home/alex/programs/
+# cd /home/alex/programs/
 
 # kibana
 filename="kibana-3.1.0.tar.gz"
