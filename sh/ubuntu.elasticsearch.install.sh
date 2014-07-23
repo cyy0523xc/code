b4f89aa306745c810816de368b0e93f8f0c7ee33
alex cai <cyy0523xc@gmail.com>
2014-07-23 14:24:12 +0800

    es

--- sh/ubuntu.elasticsearch.install.sh
+++ sh/ubuntu.elasticsearch.install.sh
@@ -11,38 +11,46 @@
 # NOTICES: 如果目录不同，则注意修改这里
 cd /home/alex/programs/
 
-# elasticsearch
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.2.tar.gz
-echo "elasticsearch download OK"
-
-tar -vxf elasticsearch*.tar.gz
-echo "elasticsearch tar OK"
-
 # kibana
-wget https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz
-echo "kibana download OK"
+filename="kibana-3.1.0.tar.gz"
+if ! [ -f $filename ]; then
+    wget https://download.elasticsearch.org/kibana/kibana/$filename
+    echo "kibana download OK"
 
-tar -vxf kibana*tar.gz
-echo "kibana tar OK"
+    tar -vxf $filename
+    echo "kibana tar OK"
+fi
 
-cd elasticsearch-1.2.2/
+# elasticsearch
+filename="elasticsearch-1.2.2.tar.gz"
+dirname="elasticsearch-1.2.2"
+if ! [ -f $filename ]; then
+    wget https://download.elasticsearch.org/elasticsearch/elasticsearch/$filename
+    echo "elasticsearch download OK"
 
-# marvel
-bin/plugin -i elasticsearch/marvel/latest
-echo "marvel OK"
+    tar -vxf $filename
+    echo "elasticsearch tar OK"
 
-# es2unix
-curl -s download.elasticsearch.org/es2unix/es > bin/es 
-chmod +x bin/es
-echo "es2unix OK"
+    cd $dirname
 
-# jetty
-# @see https://github.com/sonian/elasticsearch-jetty
-bin/plugin -url https://oss-es-plugins.s3.amazonaws.com/elasticsearch-jetty/elasticsearch-jetty-1.2.1.zip -install elasticsearch-jetty-1.2.1
-echo ""
-echo "# jetty config"
-echo "http.type: com.sonian.elasticsearch.http.jetty.JettyHttpServerTransportModule" >> config/elasticsearch.yml
-echo "elasticsearch-jetty OK"
+    # marvel
+    bin/plugin -i elasticsearch/marvel/latest
+    echo "marvel OK"
+
+    # es2unix
+    curl -s download.elasticsearch.org/es2unix/es > bin/es 
+    chmod +x bin/es
+    echo "es2unix OK"
+
+    # jetty
+    # @see https://github.com/sonian/elasticsearch-jetty
+    bin/plugin -url https://oss-es-plugins.s3.amazonaws.com/elasticsearch-jetty/elasticsearch-jetty-1.2.1.zip -install elasticsearch-jetty-1.2.1
+    echo ""
+    echo "# jetty config"
+    echo "http.type: com.sonian.elasticsearch.http.jetty.JettyHttpServerTransportModule" >> config/elasticsearch.yml
+    echo "elasticsearch-jetty OK"
+
+fi
 
 echo "---All is OK."
 
