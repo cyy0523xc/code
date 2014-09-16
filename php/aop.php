f6a27ffa969a0e8f22447262b3521e3f4130bef7
alex cai <cyy0523xc@gmail.com>
2014-09-16 12:18:26 +0800

    AOP

--- php/aop.php
+++ php/aop.php
@@ -44,7 +44,7 @@ class CyyControl extends CyyBase
  * 业务逻辑类的包装类
  * @author cyy0523xc@gmail.com
  */
-class AOP
+final class AOP
 {
     private $instance;
 
@@ -55,38 +55,55 @@ class AOP
 
     public function __call($method, $params)
     {
-        if (!$this->methodExists($method)) {
+        if (!$this->__methodExists($method)) {
             throw new Exception("Call undefinded method " . get_class($this->instance) . "::$method");
         }
 
         // 调用前置函数
-        if ($this->methodExists('before')) {
-            $this->callMethod('before');
+        if ($this->__methodExists('before')) {
+            $this->__callMethod('before');
         }
 
         // 调用业务函数
-        $return = $this->callMethod($method, $params);
+        $return = $this->__callMethod($method, $params);
 
         // 调用后置函数
-        if ($this->methodExists('after')) {
-            $this->callMethod('after');
+        if ($this->__methodExists('after')) {
+            $this->__callMethod('after');
         }
         
         return $return;
     }
 
-    private function methodExists($method)
+    /**
+     * 判断方法是否存在
+     * 使用双下划线前缀主要是为了避免冲突
+     * @param string $method 方法名
+     * @return mixed
+     */
+    private function __methodExists($method)
     {
         return method_exists($this->instance, $method);
     }
 
-    private function callMethod($method, array $params = array())
+    /**
+     * 调用方法
+     * @param string $method 方法名
+     * @param array  $params 参数数组
+     * @return mixed
+     */
+    private function __callMethod($method, array $params = null)
     {
         $callBack = array(
             $this->instance,
             $method
         );
-        return call_user_func_array($callBack, $params);
+
+        if (null === $params) {
+            return call_user_func($callBack);
+        } else {
+            return call_user_func_array($callBack, $params);
+        }
     }
 }
 
