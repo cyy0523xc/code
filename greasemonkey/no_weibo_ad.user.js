ce2393ffb82a78b42305160f918edee626904550
alex cai <cyy0523xc@gmail.com>
2014-09-24 18:43:23 +0800

    greasemonkey

---
+++ greasemonkey/no_weibo_ad.user.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name          No Weibo Advertisements
+// @namespace     http://cyy0523xc.github.io/
+// @description   删除微博的广告等多余信息。基于Cao Weibo修改
+// @include       http://www.weibo.com/*
+// @include       http://weibo.com/*
+// @copyright     2014, cyy0523xc@gmail.com
+// ==/UserScript==
+
+//  return script name
+function script_name()
+{
+    return "NoWeiboAd";
+}
+
+//  return debug flag
+function debug_flag()
+{
+    return true;
+}
+
+//  function return the result of xpath query
+function xpath(query)
+{
+    return document.evaluate(query, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+}
+
+//  common function: remove nodes
+function remove_nodes(allNode)
+{
+    //  for each forward
+    for (var i=0; i<allNode.snapshotLength; ++i)
+    {
+        var thisNode = allNode.snapshotItem(i);
+        if (debug_flag())
+        {
+            console.log(arguments.callee.name + ': thisNode: ' + thisNode);
+        }
+        if (thisNode)
+        {
+            thisNode.parentNode.removeChild(thisNode);
+        }
+    }
+}
+
+function remove_xpath(node_path)
+{
+    remove_nodes(xpath(node_path));
+}
+
+//  function
+function remove_ad()
+{
+    //  get '<div>' with attribute 'feedtype="ad"'
+    remove_xpath('//div[@feedtype="ad"]');
+    
+    remove_xpath('//div[@ad-data]');
+}
+
+// 
+function remove_other()
+{
+	var id_list = ["trustPagelet_checkin_lotteryv5", "trustPagelet_indexright_recom", "pl_rightmod_ads36", "trustPagelet_recom_memberv5", "pl_leftnav_app" ];
+    for (var i in id_list) {
+        remove_xpath('//div[@id="' + id_list[i] + '"]');
+    }
+}
+
+//  function
+function remove_feed_spread()
+{
+    //  get '<div>' with attribute 'node-type="feed_spread"'
+    remove_xpath('//div[@node-type="feed_spread"]');
+}
+
+//  main function
+function main()
+{
+    if (debug_flag())
+    {
+        console.log('Starting ' + script_name());
+    }
+    //  remove advertisements
+    remove_ad();
+    //  remove feed spread
+    remove_feed_spread();
+    // remove other
+    remove_other();
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+window.setTimeout(main, 1000);
--- vim/cyy.vim
+++ vim/cyy.vim
@@ -3,6 +3,10 @@
 " Author:   cyy0523xc@gmail.com
 "
 
+let s:cyy_plugins_path = '/home/code/github/code/vim/plugins'
+"echo s:cyy_plugins_path
+"!ls R/
+
 " 测试 
 function! Helloworld()
     echo "hello,world"
@@ -12,6 +16,16 @@ command! -nargs=0 Helloworld call Helloworld()
 " php_beautifier
 map <C-b> :% ! php_beautifier --filters "Pear() ArrayNested()"<CR>
 
+" powerline插件
+set t_Co=256
+let g:Powerline_symbols = 'unicode'
+set encoding=utf8
+
+
+"au BufWritePost * :echo "this is vim file."
+
 " 如果编辑的是blog目录下的，且后缀为md的文件
 " 在保存并退出vim时，需要自动git push并发布到网上
+"source s:cyy_plugins_path . '/blogdeploy.vim'
+source /home/code/github/code/vim/plugins/blogdeploy.vim 
 
