b539f477b02e8aab597916867c00f7185c84f54f
alex cai <cyy0523xc@gmail.com>
2014-10-23 10:27:00 +0800

    bashrc git

--- sh/ubuntu.bashrc.git.sh
+++ sh/ubuntu.bashrc.git.sh
@@ -14,18 +14,43 @@ gitsub() {
         "add")
             git subtree add --prefix=$2 $2 $3 --squash
             ;;
-        *)
+        "remote")
+            git remote add -f $2 $3
+            ;;
+        "h"|"help"|*)
             cat <<EOF
 params is error!
 
 USAGES:
 
-gitsub add \$dir \$branch 
-gitsub pull \$dir 
-gitsub push \$dir \$branch
+gitsub remote \$dir \$git_url
+gitsub add    \$dir \$branch
+gitsub pull   \$dir
+gitsub push   \$dir \$branch
+gitsub help
 EOF
             ;;
     esac
 
 }
 
+gitsub_complete() {
+    COMPREPLY=()
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local com=${COMP_WORDS[COMP_CWORD-1]}
+    
+    case $com in 
+        'gitsub')
+            COMPREPLY=($(compgen -W 'add pull push remote help' -- $cur))
+            ;;
+        'compile')
+            local pro=($(awk '{print $1}' project.list))
+            COMPREPLY=($(compgen -W '${pro[@]}' -- $cur))
+            ;;
+        *)
+            ;;
+    esac
+    return 0
+}
+    
+complete -F gitsub_complete gitsub 
