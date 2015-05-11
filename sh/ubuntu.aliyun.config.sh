e93feb9c730807e184aaa9b0ac27d653cf97e345
alex cai <cyy0523xc@gmail.com>
2015-05-11 12:51:16 +0800

    aliyun

--- sh/ubuntu.aliyun.config.sh
+++ sh/ubuntu.aliyun.config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # 在阿里云上配置laravel运行环境
+# wget https://raw.githubusercontent.com/cyy0523xc/code/master/sh/ubuntu.aliyun.config.sh -O ubuntu.aliyun.config.sh | bash 
+
+# install curl 
+apt-get install curl
 
 # install php
 apt-get install libapache2-mod-php5 php-apc php-pear php5  php5-cli php5-common php5-curl php5-dev php5-fpm php5-gd php5-imagick php5-mysqlnd php5-mcrypt php5-msgpack php5-mysql
