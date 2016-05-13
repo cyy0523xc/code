67bd1aea380349cb7f454172600c4517cf12de9b
alex cai <caiyingyao@ibbd.net>
2016-05-13 18:02:22 +0800

    restart sogou

--- sh/limit_fcitx.sh
+++ sh/limit_fcitx.sh
@@ -6,12 +6,10 @@ do
 done
 
 limit_fcitx_sh=/var/www/github/cyy-code/sh/limit_fcitx.sh
-limit_fcitx_sh_p="${limit_fcitx_sh//\//\\\/}"
-echo $limit_fcitx_sh_p
 if grep "limit_fcitx.sh" /etc/rc.local; then
-    "limit_fcitx.sh had beed in /etc/rc.local"
+    echo "limit_fcitx.sh is in /etc/rc.local"
 else
     sed -i "s/^exit 0$/\nexit 0/" /etc/rc.local
-    sed -i "s/^exit 0$/\n# By limit_fcitx.sh\nexit 0/" /etc/rc.local
-    sed -i "s/^exit 0$/\nnohup $limit_fcitx_sh_p &\nexit 0/" /etc/rc.local
+    sed -i "s/^exit 0$/# By alex\nexit 0/" /etc/rc.local
+    sed -i "s|^exit 0$|nohup $limit_fcitx_sh \&\nexit 0|" /etc/rc.local
 fi
