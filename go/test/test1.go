610bea4ad6e5ed00f5f4cd24ee70842496ed9e80
alex cai <caiyingyao@ibbd.net>
2016-05-20 18:00:24 +0800

    golang

---
+++ go/test/test1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"log"
+	"net/http"
+)
+
+func main() {
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
+		w.WriteJson(map[string]string{"Body": "Hello World!"})
+	}))
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
