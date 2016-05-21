7f664bccdac9a580b5b5c087e8eb4e9ab26896fc
alex cai <caiyingyao@ibbd.net>
2016-05-22 00:26:34 +0800

    golang

--- go/test/test4.go
+++ go/test/test4.go
@@ -1,5 +1,15 @@
 package main
 
+/**
+curl -i -H 'Content-Type: application/json' \
+    -d '{"Name":"Antoine"}' http://127.0.0.1:8080/users
+curl -i http://127.0.0.1:8080/users/0
+curl -i -X PUT -H 'Content-Type: application/json' \
+    -d '{"Name":"Antoine Imbert"}' http://127.0.0.1:8080/users/0
+curl -i -X DELETE http://127.0.0.1:8080/users/0
+curl -i http://127.0.0.1:8080/users
+*/
+
 import (
 	"fmt"
 	"github.com/ant0ine/go-json-rest/rest"
