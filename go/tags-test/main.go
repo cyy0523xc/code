f5c611421cd7a68c8ac41d1e848041da274a0b4f
alex cai <caiyingyao@ibbd.net>
2016-09-08 10:12:44 +0800

    golang tag

---
+++ go/tags-test/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type TagType struct {
+	field1 bool   "An important answer"
+	field2 string `hello:"The name of the thing" world:""`
+	field3 int    `how much there are`
+	field4 int    `hello:"hello value" world:"world value"`
+}
+
+func main() {
+	tt := TagType{true, "Barak Obama", 1, 100}
+	var reflectType reflect.Type = reflect.TypeOf(tt)
+	var field reflect.StructField
+	for i := 0; i < reflectType.NumField(); i++ {
+		field = reflectType.Field(i)
+		fmt.Printf("%+v\n", field)
+		fmt.Printf("%s\n", field.Tag)
+		fmt.Printf("%s\n", field.Type)
+
+		if i == 3 {
+			fmt.Printf("hello: %s\n", field.Tag.Get("hello"))
+			fmt.Printf("world: %s\n", field.Tag.Get("world"))
+		}
+
+		if world, ok := field.Tag.Lookup("world"); ok {
+			if world == "" {
+				fmt.Println("(blank)")
+			} else {
+				fmt.Println(world)
+			}
+		} else {
+			fmt.Println("(not specified)")
+		}
+	}
+}
