ebde41bb56f973726dd6835a2e834c5e5b35d015
alex cai <caiyingyao@ibbd.net>
2016-11-30 15:03:11 +0800

    scrapy

--- python/scrapy/google/google/spiders/google.py
+++ python/scrapy/google/google/spiders/google.py
@@ -18,8 +18,8 @@ class GoogleSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        r = response.xpath("//div").extract()
+        print("Length of div: %d" % len(r))
+
+        r = response.xpath("//div[@class='g']").extract()
+        print("Length of div g: %d" % len(r))
