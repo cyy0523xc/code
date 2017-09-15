b34429b7752365ea5d7a90fabc01dc98f4b5f638
alex cai <caiyingyao@ibbd.net>
2017-09-15 22:55:36 +0800

    fasttext

--- python/fastText/format_train_file.py
+++ python/fastText/format_train_file.py
@@ -6,34 +6,47 @@
 import csv
 import jieba
 
-src = '/home/alex/data/hsbianma_page10_sogou_huaxue.csv'
-train_file = '/home/alex/data/hsbianma_page10_sogou_huaxue.fasttext.train.txt'
-test_file = '/home/alex/data/hsbianma_page10_sogou_huaxue.fasttext.test.txt'
+src = 'news.csv'
+train_csv = 'news.train.csv'
+test_csv = 'news.test.csv'
+train_file = 'fasttext.train.txt'
+test_file = 'fasttext.test.txt'
 
 
-def format_train_file(src, train_file, test_file):
-    with open(src) as r, open(train_file, 'w') as w_train, open(test_file, 'w') as w_test:
-        csv_r = csv.DictReader(r, fieldnames=('kind', 'content'))
+def format_train_file(src, train_file, test_file, train_csv_file, test_csv_file):
+    with open(src) as r, \
+            open(train_csv_file, 'w') as w_train_csv_f, \
+            open(test_csv_file, 'w') as w_test_csv_f, \
+            open(train_file, 'w') as w_train, \
+            open(test_file, 'w') as w_test:
         i = 0
-        words_set = set()
+        csv_r = csv.DictReader(r)
+        fieldnames = ('id', 'kind', 'content')
+        train_csv = csv.DictWriter(w_train_csv_f, fieldnames=fieldnames)
+        test_csv = csv.DictWriter(w_test_csv_f, fieldnames=fieldnames)
+        train_csv.writeheader()
+        test_csv.writeheader()
+        sep_char = ""
         for row in csv_r:
-            row['content'] = row['content'].strip()
-            if "\n" in row['content']:
+            if not row['id'].isnumeric():
                 continue
 
+            row['content'] = row['content'].strip()
             words = jieba.cut(row['content'])
-            if (row['kind'], row['content']) in words_set:
-                continue
-            words_set.add((row['kind'], row['content']))
+            words = [w for w in words if "\n" not in w]
 
             i += 1
             if i % 10 == 0:
-                w_test.write('__label__' + row['kind'][:4] + ' ' + ' '.join(words) + "\n")
+                w_test.write(sep_char+'__label__' + row['kind'] + ' ' + ' '.join(words))
+                test_csv.writerow(row)
             else:
-                w_train.write('__label__' + row['kind'][:4] + ' ' + ' '.join(words) + "\n")
+                w_train.write(sep_char+'__label__' + row['kind'] + ' ' + ' '.join(words))
+                train_csv.writerow(row)
+
+            sep_char = "\n"
 
 
 # 格式化训练文件
-format_train_file(src, train_file, test_file)
+format_train_file(src, train_file, test_file, train_csv, test_csv)
 
 print("ok")
--- python/fastText/train.py
+++ python/fastText/train.py
@@ -3,13 +3,26 @@
 #
 # Author: alex
 # Created Time: 2017年09月10日 星期日 13时18分07秒
+import csv
+import jieba
 import fasttext
 
-train_file = '/home/alex/data/hsbianma_page10_sogou_huaxue.fasttext.train.txt'
-test_file = '/home/alex/data/hsbianma_page10_sogou_huaxue.fasttext.test.txt'
+train_file = 'fasttext.train.txt'
+test_file = 'fasttext.test.txt'
 
 # 训练
-classifier = fasttext.supervised(train_file, 'classify_model')
+classifier = fasttext.supervised(train_file, 'classify_model',
+                                 lr=1.0,
+                                 epoch=30,
+                                 #word_ngrams=2
+                                 loss='hs'
+                                 )
+
+# 测试
+result = classifier.test(train_file)
+print('P@1:', result.precision)
+print('R@1:', result.recall)
+print('Number of examples:', result.nexamples)
 
 # 测试
 result = classifier.test(test_file)
@@ -17,25 +30,30 @@ print('P@1:', result.precision)
 print('R@1:', result.recall)
 print('Number of examples:', result.nexamples)
 
+
 def test(test_file):
-    pre_len = len('__label__2924')
     with open(test_file) as r:
         total = 0
-        topn_num = 5
+        topn_num = 1
         topn = [0] * topn_num
-        for row in r.readlines():
-            row = row.strip()
-            kind = row[pre_len-4:pre_len]
-            words = row[pre_len+1:]
-            res = classifier.predict([words], k=topn_num)
+        cr = csv.DictReader(r)
+        for row in cr:
+            words = jieba.cut(row['content'])
+            words = [w for w in words if "\n" not in w]
+            if len(words) < 1:
+                continue
+
+            res = classifier.predict([" ".join(words)], k=topn_num)
             total += 1
             for i in range(topn_num):
-                if kind == res[0][i]:
+                if row['kind'] == res[0][i]:
                     topn[i] += 1
 
         topn = [i/total for i in topn]
         print("Total: %d   topn:" % total, topn, sum(topn))
 
 
-test(train_file)
-test(test_file)
+train_file = 'news.train.csv'
+test_file = 'news.test.csv'
+#test(train_file)
+#test(test_file)
