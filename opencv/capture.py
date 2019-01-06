4a054148f34295d70e29086473b2fdc40d913d33
alex cai <cyy0523xc@gmail.com>
2019-01-06 10:56:53 +0800

    fix capture save

--- opencv/capture.py
+++ opencv/capture.py
@@ -5,12 +5,14 @@
 # Created Time: 2019年01月06日 星期日 10时36分05秒
 import cv2
 
-outfile = 'output.avi'
 cap = cv2.VideoCapture(0)
-fourcc = cv2.cv.CV_FOURCC("D", "I", "B", " ")
+# outfile = 'output.mp4'
+# fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+outfile = 'output.avi'
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
 # 第三个参数则是镜头快慢的，20为正常，小于二十为慢镜头
-out = cv2.VideoWriter(outfile, fourcc, 3.0, (640, 480))
+out = cv2.VideoWriter(outfile, fourcc, 20.0, (640, 480))
 while cap.isOpened():
     ret, frame = cap.read()
     if ret is False:
