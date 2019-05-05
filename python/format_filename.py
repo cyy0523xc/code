38ce6e238347365e1e9e11c0ac314a35a711a479
alex <cyy0523xc@gmail.com>
2019-05-05 18:35:13 +0800

    py

---
+++ python/format_filename.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+#
+# Author: alex
+# Created Time: 2019年05月05日 星期日 17时16分08秒
+import os
+import shutil
+from imutils.paths import list_images
+
+
+def move(path):
+    for pf in list_images(path):
+        f = pf.split('/')[-1]
+        new_f = f[0] + ("%05d" % int(f[1:-4])) + f[-4:]
+        shutil.move(pf, os.path.join(path, new_f))
+
+
+move('/home/alex/文档/汕头/data/out20190504_02')
